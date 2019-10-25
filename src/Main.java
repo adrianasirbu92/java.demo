7ac46bf0e47d83c872beb1fce40e66e99d4f6234
adrianasirbu92 <56941428+adrianasirbu92@users.noreply.github.com>
2019-10-25 15:10:24 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.sql.SQLOutput;
+
 public class Main {
    
     public static void main(String[] args) {
-       System.out.println("Hello World");
+        for (int i = 0; i <10; i++) {
+            if (i==4){
+                continue;
+            }
+            System.out.println(i);
+
+        }
+
     }
 }
